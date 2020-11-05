4b170b0cfb4c317674b2d3b963afaf19fb2bbe7c
Felipe Kinoshita <kinofhek@gmail.com>
2020-11-05 12:38:52 -0300

    refactor: move vscode repo to software.sh

--- fedora.sh
+++ fedora.sh
@@ -29,17 +29,6 @@ sudo dnf install fontconfig-enhanced-defaults fontconfig-font-replacements -yq
 # add flathub repository
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# add third party software
-
-# vscode
-
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc --quiet
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-
-# update repositories
-
-sudo dnf check-update -yq
-
 # install software
 ./software.sh
 
--- software.sh
+++ software.sh
@@ -22,6 +22,15 @@ FLATPAK_LIST=(
 	org.gnome.Podcasts
 )
 
+# add third party software
+
+# vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc --quiet
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+# update repositories
+sudo dnf check-update -yq
+
 # iterate through packages and installs them if not already installed
 for package_name in ${PACKAGE_LIST[@]}; do
 	if ! sudo dnf list --installed | grep -q "^\<$package_name\>"; then
