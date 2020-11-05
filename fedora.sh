b3eadb647a5b30e764ef91a9390e4f93793a25cc
Felipe Kinoshita <kinofhek@gmail.com>
2020-11-04 21:24:50 -0300

    feat: disable background logo extension

--- fedora.sh
+++ fedora.sh
@@ -9,6 +9,9 @@ gsettings set org.gnome.desktop.interface clock-format 12h
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic true
 
+# disable background logo extension
+gnome-extensions disable background-logo@fedorahosted.org
+
 # enable rpmfusion
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -yq
 
