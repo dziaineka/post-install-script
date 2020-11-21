b3d36e16909292114cb710889f8535f9a7b5a786
Felipe Kinoshita <kinofhek@gmail.com>
2020-11-20 23:48:53 -0300

    refactor: edit/remove comments

--- fedora.sh
+++ fedora.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
-# undo vim bind to vi
 unalias vim
 
-# gnome settings
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 gsettings set org.gnome.desktop.interface clock-format 12h
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic true
 
-# change mouse profile to flat
 gsettings set org.gnome.desktop.peripherals.mouse accel-profile flat
 
-# disable background logo extension
 gnome-extensions disable background-logo@fedorahosted.org
 
 # enable rpmfusion
@@ -29,17 +25,13 @@ sudo dnf groupupdate sound-and-video -yq
 sudo dnf copr enable dawid/better_fonts -yq
 sudo dnf install fontconfig-enhanced-defaults fontconfig-font-replacements -yq
 
-# add flathub repository
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# install software
 ./software.sh
 
-# disable services
 sudo systemctl disable NetworkManager-wait-online.service
 sudo systemctl disable lvm2-monitor.service
 sudo systemctl disable ModemManager.service
 
-# upgrade packages
 sudo dnf upgrade -yq
 sudo dnf autoremove -yq
--- software.sh
+++ software.sh
@@ -18,16 +18,12 @@ FLATPAK_LIST=(
 	com.discordapp.Discord
 )
 
-# add third party software
-
-# vscode
+# add vscode repository
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc --quiet
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
-# update repositories
 sudo dnf check-update -yq
 
-# iterate through packages and installs them if not already installed
 for package_name in ${PACKAGE_LIST[@]}; do
 	if ! sudo dnf list --installed | grep -q "^\<$package_name\>"; then
 		echo "installing $package_name..."
