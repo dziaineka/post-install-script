5dc6c1c364feb2a89a0a140ad1e20a743f162c6b
Felipe Kinoshita <kinofhek@gmail.com>
2020-12-04 01:20:41 -0300

    feat: change login shell to zsh

--- fedora.sh
+++ fedora.sh
@@ -20,6 +20,8 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 
 ./software.sh
 
+chsh -s /usr/bin/zsh
+
 sudo systemctl disable NetworkManager-wait-online.service
 sudo systemctl disable lvm2-monitor.service
 sudo systemctl disable ModemManager.service
