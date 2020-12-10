921ce705751805f52de02b2b60c6359aae42b507
Felipe Kinoshita <kinofhek@gmail.com>
2020-12-10 00:27:59 -0300

    feat: remove quiet option on init.sh

--- init.sh
+++ init.sh
@@ -3,18 +3,18 @@
 unalias vim
 
 # enable rpmfusion
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -yq
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
-sudo dnf groupupdate core -yq
+sudo dnf groupupdate core -y
 
 # install multimedia packages
-sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -yq
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
 
-sudo dnf groupupdate sound-and-video -yq
+sudo dnf groupupdate sound-and-video -y
 
 # fedora better fonts
-sudo dnf copr enable dawid/better_fonts -yq
-sudo dnf install fontconfig-enhanced-defaults fontconfig-font-replacements -yq
+sudo dnf copr enable dawid/better_fonts -y
+sudo dnf install fontconfig-enhanced-defaults fontconfig-font-replacements -y
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
@@ -28,11 +28,10 @@ sudo systemctl disable NetworkManager-wait-online.service
 sudo systemctl disable lvm2-monitor.service
 sudo systemctl disable ModemManager.service
 
-sudo dnf upgrade -yq
-sudo dnf autoremove -yq
+sudo dnf upgrade -y
+sudo dnf autoremove -y
 
 # run get-config script
 git clone https://github.com/fhek789/get-config.git
 cd get-config
 ./init.sh
-
