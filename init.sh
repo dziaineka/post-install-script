be1f9af861e42c1dbea90792f042f833ae6aa2f5
Felipe Kinoshita <kinofhek@gmail.com>
2020-12-22 22:32:56 -0300

    feat: remove 'unalias vim' since no longer needed

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-unalias vim
-
 # enable rpmfusion
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
