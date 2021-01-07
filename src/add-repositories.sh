3cee06cacf679c6fb72e77f8ca325dbeea2f7de3
Andrei Dziaineka <dziaineka@gmail.com>
2021-01-07 16:38:16 +0300

    sublime repo added

--- src/add-repositories.sh
+++ src/add-repositories.sh
@@ -4,4 +4,8 @@
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc --quiet
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
+# add sublime repository
+sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+
 sudo dnf check-update -yq
