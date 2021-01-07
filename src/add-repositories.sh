74fb1d6f9b96d8a6b21d22a850bfe903776e44e0
Andrei Dziaineka <dziaineka@gmail.com>
2021-01-07 22:17:08 +0300

    anydesk repo added

--- src/add-repositories.sh
+++ src/add-repositories.sh
@@ -8,4 +8,16 @@ sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.m
 sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
 sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 
+# add anydesk repository
+sudo su
+cat > /etc/yum.repos.d/AnyDesk-Fedora.repo << "EOF"
+[anydesk]
+name=AnyDesk Fedora - stable
+baseurl=http://rpm.anydesk.com/fedora/$basearch/
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://keys.anydesk.com/repos/RPM-GPG-KEY
+EOF
+exit
+
 sudo dnf check-update -yq
